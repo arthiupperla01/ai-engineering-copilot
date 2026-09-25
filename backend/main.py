40c2f45bb26b95a854753016a26fb0a2fa4b0618
Upperla Arthi <arthiu@agilitegroup.com>
2026-09-25 15:30:24 +0530

    feat: connect application configuration

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from backend.config import settings
+
 app = FastAPI()
+
+
+@app.get("/")
+def root() -> dict[str, str]:
+    return {
+        "app": settings.app_name,
+        "environment": settings.environment,
+    }
 
 
 @app.get("/health")
